47bcf159e2cd1a25cc91b3b8b49c9a15f8844b18
blotin1993 <blottagonzalo@gmail.com>
2021-06-03 11:00:02 -0300

    adding handlers and server configuration

---
+++ handlers/goodbye.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodBye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	g.l.Println("Byeeee")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw, "Goodbye %s", d)
+}
